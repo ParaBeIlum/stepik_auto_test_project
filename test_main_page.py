207a5f5180839acd765175641c58b3b1e16c48e4
flash <biotexholog@gmail.com>
2021-12-17 23:18:19 +0300

    fixed find_element()

--- test_main_page.py
+++ test_main_page.py
@@ -5,7 +5,7 @@ import time
 link = "http://selenium1py.pythonanywhere.com/"
 
 def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
+    login_link = browser.find_element(By.CSS_SELECTOR, '#login_link')
     login_link.click()
 
 def test_guest_can_go_to_login_page(browser): 
